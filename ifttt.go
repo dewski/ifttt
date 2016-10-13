bc62d459b95b6e51334978ee69256da7b7fe2dc4
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2016-10-13 16:25:14 -0700

    Initial commit

---
+++ ifttt.go
@@ -0,0 +1,48 @@
+package ifttt
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Values struct {
+	Value1 string `json:"value1"`
+	Value2 string `json:"value2"`
+	Value3 string `json:"value2"`
+}
+
+var (
+	urlFormat = "https://maker.ifttt.com/trigger/%s/with/key/%s"
+	makerKey  = os.Getenv("IFTTT_MAKER_KEY")
+)
+
+func SetKey(key string) {
+	makerKey = key
+}
+
+func Deliver(action string, values Values) error {
+	url := fmt.Sprintf(urlFormat, action, makerKey)
+	body, err := json.Marshal(values)
+	if err != nil {
+		return err
+	}
+
+	buffer := bytes.NewBuffer(body)
+	req, err := http.NewRequest("POST", url, buffer)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	_, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
